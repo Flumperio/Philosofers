e1d416c3ab040b8e9005c9729dfe9ded8787566d
Juan Carlos <juasanto@student.42madrid.com>
2022-04-10 16:31:55 +0200

    Philosofers - 10-04-22 16:31

--- test.sh
+++ test.sh
@@ -1,8 +1,76 @@
 #! /bin/bash
 make re
-echo "Philo 5 100 10 20 50"
-./philo 5 100 20 20 50 | grep "Philo{1} is eating."
-./philo 5 100 20 20 50 | grep "Philo{2} is eating."
-./philo 5 100 20 20 50 | grep "Philo{3} is eating."
-./philo 5 100 20 20 50 | grep "Philo{4} is eating."
-./philo 5 100 20 20 50 | grep "Philo{5} is eating."
+echo "Philo 5 100 100 20"
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+./philo 5 100 100 20
+
